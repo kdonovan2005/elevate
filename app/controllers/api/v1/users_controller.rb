da4dddc33fc9e4e0f4dc22f7e82865c5f8a44625
steve21168 <steve21168@gmail.com>
2016-06-15 19:14:33 -0400

    put serializers in serializer folder, fixed syntax errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,9 +3,13 @@ module Api
     class UsersController < ApplicationController
 
       def index
-        render json: User.includes(:list_items), include: ['goals', 'list_items']
+        render json: User.includes(:list_items).all, include: ['goals', 'list_items']
       end
 
+      def show
+        user = User.includes(:list_items).find(params[:id])
+        render json: user, include: ['goals', 'list_items']
+      end
     end
   end
 end
