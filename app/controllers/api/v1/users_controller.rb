a502cf1e24b0e0c2462bd218dc7cecb42c1e8243
kdonovan2005 <kdonovan2005@gmail.com>
2016-06-17 20:26:15 -0400

    sort of adding list items

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,26 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      skip_before_action :authenticate, only: [:create]
 
       def show
         user = User.includes(:list_items).find(params[:id])
         render json: user, include: ['goals', 'list_items']
       end
 
+      def create
+        binding.pry
+        user = User.create(user_params)
+        render json: user
+      end
+
+      private
+      
+      def user_params
+        params.require(:data).require(:attributes).permit(:name, :email, :avatar, :password)
+      end
+
     end
+
   end
 end
