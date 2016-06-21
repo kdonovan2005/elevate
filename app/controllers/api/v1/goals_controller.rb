ccb6353b2e4e4e79c052535f46f4d45272a5261c
steve21168 <steve21168@gmail.com>
2016-06-20 21:09:47 -0400

    goals have update in controller to change status

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -15,12 +15,24 @@ module Api
         goal_name = params["data"]["attributes"]["name"]
         start_date = params["data"]["attributes"]["start-date"]
         end_date = params["data"]["attributes"]["end-date"]
-        goal = Goal.create(name: goal_name, start_date: start_date, end_date: end_date, user: current_user)
+        goal = Goal.create(name: goal_name, start_date: start_date, end_date: end_date, user: current_user, active: true)
+        render json: goal
+      end
+
+      def update
+        goal = Goal.find(params["data"]["id"])
+        goal.update(goal_params)
         render json: goal
       end
 
       def destroy
         Goal.find(params["id"]).destroy
+      end
+
+      private
+
+      def goal_params
+        params.require("data").require("attributes").permit("name", "active")
       end
 
     end
